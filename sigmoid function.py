2f8b214fd1e482e93ec9b9432b8d32ff94c90325
Ayush srivastava <11ayush11@gmail.com>
2018-02-25 14:29:21 +0530

    Create sigmoid function.py

---
+++ sigmoid function.py
@@ -0,0 +1,20 @@
+# this is a built in scratch code for sigmoid function that is used in neural network.
+# assume we have a dataset of 2features and an output for each input.
+
+import numpy as np
+def Neural_network(m1,m2,w1,w2,b):       #m1 and m2 are two features and w1 and w2 are randomly allocated weight and b is a bias variable
+    z= m1*w1 + m2*w2 + b;
+    return sigmoid (z)
+    
+def sigmoid(z):
+    return = (1/(1+numpy.exp(-x))
+    
+    
+# WE apply random function to choose w1 , w2 and b then w try to tune the parameters.
+w1 = numpy.random.randn()
+w2 = numpy.random.randn()
+b = numpy.random.randn()
+
+
+    
+    
